1eb1cdeb67a3e545a8ee13ee229900a208ea83e6
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-14 16:08:20 +0100

    plan tests to be written

---
+++ spec/user/user.model.spec.js
@@ -0,0 +1,26 @@
+describe('Database Test for user service', function(){
+  beforeEach(function (done) {
+
+  });
+
+  it('Test for Database and table creation', function (done) {
+
+  });
+
+  it('Test for requirements', function (done) {
+
+  });
+
+  it('Test for Email format', function (done) {
+
+  });
+
+  it ('Test for ID generation', function (done) {
+
+  });
+
+  it('Test for unique username', function (done) {
+
+  });
+
+});
