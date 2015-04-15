aba2b75498af7dedcf1b66b6c4cfa1362deb1383
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-15 15:58:00 +0100

    write configuration for knex connection

---
+++ config/config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  db: {
+    development: {
+
+      pg: {
+        client: 'pg',
+        connection: {
+          host     : '127.0.0.1',
+          user     : 'Yinka',
+          password : '',
+          database : 'users-db'
+        }
+      }
+    },
+    test: {
+
+      pg: {
+        client: 'pg',
+        connection: {
+          host     : '127.0.0.1',
+          user     : 'Yinka',
+          password : '',
+          database : 'users-db'
+        }
+      }
+    },
+    production: {
+
+      pg: {
+        client: 'mysql',
+        connection: {
+          host     : '127.0.0.1',
+          user     : 'Yinka',
+          password : '',
+          database : 'users-db'
+        }
+      }
+    }
+  },
+
+  port: process.env.PORT || 3000
+
+};
