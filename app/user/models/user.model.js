30b1b8c4f2bebcd02fb457a5fb5c453eff1bfc44
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-15 16:53:13 +0100

    drop database table if exists log table created to console throw error if table can not be created

--- app/user/models/user.model.js
+++ app/user/models/user.model.js
@@ -1,19 +1,28 @@
-var knex = require('knex'),
-    bookshelf = require('bookshelf');
+var knex = require('knex')(config.db[process.env.NODE_ENV].pg),
+    bookshelf = require('bookshelf')(knex);
 
-
-bookshelf.knex.schema.createTable('users', function (table) {
-  table.increments('id').notNullable().primary();
-  table.string('first_name').notNullable();
-  table.string('last_name').notNullable();
-  table.string('email').notNullable().unique();
-  table.string('username').notNullable().unique();
-  table.string('password').notNullable();
-  table.timestamps();
+bookshelf.knex.schema.dropTableIfExists('users').then(function () {
+  console.log('table dropped');
+  return 
+    bookshelf.knex.schema.createTable('users', function (table) {
+      table.increments('id').notNullable().primary();
+      table.string('first_name').notNullable();
+      table.string('last_name').notNullable();
+      table.string('email').notNullable().unique();
+      table.string('username').notNullable().unique();
+      table.string('password').notNullable();
+      table.timestamps();
+    })
+    .then(function () {
+      console.log('table created!');
+    })
+    .otherwise(function (error) {
+      throw error;
+    });
 });
 
 var Users = bookshelf.Model.extend({
-  tablename: 'users'
+  tableName: 'users'
 });
 
 module.exports = Users;
