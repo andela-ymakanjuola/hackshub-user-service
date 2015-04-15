f515a71768f584a65f643daacdb3ab923aaca1e8
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-15 14:18:35 +0100

    write express app

---
+++ config/express.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+   
+module.exports = function () {
+  var app = express(),
+      bodyParser = require('body-parser'),
+      router = require('.././app/user/routes/user.route');
+
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+
+  app.use('/users',router);
+
+  return app;
+  
+};
