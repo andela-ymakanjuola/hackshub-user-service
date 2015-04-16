78fefc1e162739949432ce8af76ee9a0eb3a3d57
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-16 17:46:47 +0100

    write create user callback function

--- app/user/controllers/user.controller.js
+++ app/user/controllers/user.controller.js
@@ -1,8 +1,23 @@
 User = require('.././models/user.model');
 
-modulde.exports = {
+module.exports = {
 
   create: function (request, response) {
+
+    if(!(request.body.username && request.body.password && request.body.email)){
+      response.json('Username and Password required');
+    }
+
+    new User({
+      username: request.body.username,
+      password: request.body.password,
+      email: request.body.email
+    })
+      .fetch()
+      .then(function (User) {
+        response.json('User exists');
+      });
+
     User.forge({
       first_name: request.body.first_name,
       last_name: request.body.last_name,
@@ -15,13 +30,13 @@ modulde.exports = {
         console.log(error);
         response.save(error)
       }
-      response.json(User)
+      response.json(User);
     });
 
   },
 
   read: function (request, response) {
-    User.fetch()
+    
 
   },
 
