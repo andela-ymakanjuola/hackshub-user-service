e6c573c8295f29759f8ead638c3769646ae06e10
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-16 17:50:59 +0100

    change create table creation from dropTableIfExist to hasTable

--- app/user/models/user.model.js
+++ app/user/models/user.model.js
@@ -2,24 +2,25 @@ var config = require('../../../config/config'),
     knex = require('knex')(config.db[process.env.NODE_ENV].pg),
     bookshelf = require('bookshelf')(knex);
 
-bookshelf.knex.schema.dropTableIfExists('users').then(function () {
-  console.log('table dropped');
-  return 
-    bookshelf.knex.schema.createTable('users', function (table) {
-      table.increments('id').notNullable().primary();
-      table.string('first_name').notNullable();
-      table.string('last_name').notNullable();
-      table.string('email').notNullable().unique();
-      table.string('username').notNullable().unique();
-      table.string('password').notNullable();
-      table.timestamps();
-    })
-    .then(function () {
-      console.log('table created!');
-    })
-    .otherwise(function (error) {
-      throw error;
-    });
+bookshelf.knex.schema.hasTable('users').then(function (exists) {
+    console.log('creating table');
+  if(!exists){
+      bookshelf.knex.schema.createTable('users', function (table) {
+        table.increments('id').notNullable().primary();
+        table.string('first_name').notNullable();
+        table.string('last_name').notNullable();
+        table.string('email').notNullable().unique();
+        table.string('username').notNullable().unique();
+        table.string('password').notNullable();
+        table.timestamps();
+      })
+      .then(function () {
+        console.log('table created!');
+      })
+      .otherwise(function (error) {
+        throw error;
+      });
+  }
 });
 
 var Users = bookshelf.Model.extend({
