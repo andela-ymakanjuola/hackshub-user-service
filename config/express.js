7399fab981393c430ea007223827775c1a23faef
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-20 18:10:59 +0100

    require and use express-jwt middleware

--- config/express.js
+++ config/express.js
@@ -3,10 +3,14 @@ var express = require('express');
 module.exports = function () {
   var app = express(),
       bodyParser = require('body-parser'),
+      expressJwt = require('express-jwt'),
+      config = require('./config'),
       router = require('.././app/user/routes/user.route');
 
   app.use(bodyParser.urlencoded({extended: true}));
   app.use(bodyParser.json());
+
+  app.use(expressJwt({secret: config.secret}).unless({path:['/login']}))
 
   app.use('/',router);
 
