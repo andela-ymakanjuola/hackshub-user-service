05284565ac2b20a72cdee7c98833894b8ed760cf
Yinka Makanjuola <Yinka@Onis-MacBook-Pro.local>
2015-04-21 15:23:02 +0100

    change datatbase for development environment

--- config/config.js
+++ config/config.js
@@ -7,7 +7,7 @@ module.exports = {
           host     : 'localhost',
           user     : 'Yinka',
           password : '',
-          database : 'users-test-db'
+          database : 'users-db'
         }
       }
     },
